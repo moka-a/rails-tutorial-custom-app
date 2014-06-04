063ddb31720ea004b96f3f1e818a7bd9133d33ac
moka-a <ssgs1027@nate.com>
2014-06-04 06:02:06 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+MokaApp::Application.config.secret_key_base = '596fc1606b5503a8260e1e78c5591cefbd7921878ad23591747046c558af5f89c95a089fa2d399e52d3e4b6fbb71fac3bad9862668d19568ba5c8279567571fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MokaApp::Application.config.session_store :cookie_store, key: '_moka_app_session'
