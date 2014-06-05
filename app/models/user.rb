d25c191ff5effa98dea7aff28c92aa62787d9afa
moka-a <ssgs1027@nate.com>
2014-06-05 09:35:31 -0700

    make User model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+	before_save { self.userid = userid.downcase }
+
+	validates :userid, presence: true, length: { maximum: 30 }, uniqueness: { case_sensitive: false }
+	validates :name, presence: true, length: { maximum: 30 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+	has_secure_password
+	validates :password, length: { minimum: 6 }
+end
