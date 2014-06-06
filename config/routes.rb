f31c9f022848b389be31963f1c7e7bfef8e6cf74
moka-a <ssgs1027@nate.com>
2014-06-06 07:06:13 -0700

    sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 MokaApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   root  'static_pages#home'
   match '/signup',   to: 'users#new',             via: 'get'
+  match '/signin',   to: 'sessions#new',          via: 'get'
+  match '/signout',  to: 'sessions#destroy',      via: 'delete'
   match '/board',    to: 'static_pages#board',    via: 'get'
   match '/mypage',   to: 'static_pages#MyPage',   via: 'get'
 
