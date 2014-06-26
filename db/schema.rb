0fb69fe4e28562379a48c260f0ea465e316e6a22
moka-a <ssgs1027@nate.com>
2014-06-25 18:27:04 -0700

    make boad -ing

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140609060136) do
 
-  create_table "boardrows", force: true do |t|
-    t.string   "subject"
-    t.string   "name"
-    t.string   "memo"
-    t.integer  "hits"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
